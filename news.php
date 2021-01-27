7ca906f47ffe6699c2c5a2c141190c282139b126
Jacob Marnil <jhmarnil@gmail.com>
2021-01-27 10:12:09 +0100

    @Added News

---
+++ news.php
@@ -0,0 +1,17 @@
+<?php require("includes/core.php"); ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Find alle dine favorit film | IMDB2</title>
+        <link rel="stylesheet" href="/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/css/custom.css">
+    </head>
+    <body>
+<?php include("includes/navbar.php"); ?>
+        <div class = "container">
+            
+        </div>
+    </body>
+    <script src="/js/jquery.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+</html>
