2e7cd65c844db4894b99020e16b47f222dcf5466
Mikkel <Mikkel@DESKTOP-H8318GU>
2021-01-27 09:03:38 +0100

    @Added js and css

---
+++ Includes/core.php
@@ -0,0 +1,10 @@
+<?php
+require(realpath($_SERVER["DOCUMENT_ROOT"])."/includes/config.php");
+require(realpath($_SERVER["DOCUMENT_ROOT"])."/includes/classes/database.php");
+
+$Database = null;
+try{
+  $Database = new Database($DatabaseConnection["host"], $DatabaseConnection["username"], $DatabaseConnection["password"], $DatabaseConnection["database"]);
+}catch(Exception $error){
+  print_r($error->getMessage());
+}
--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-<?php
+<?php require("includes/core.php"); ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Find alle dine favorit film | IMDB2</title>
+        <link rel="stylesheet" href="/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/css/custom.css">
+    </head>
+    <body>
+<?php include("includes/navbar.php"); ?>
+        Test
+    </body>
+    <script src="/js/jquery.min.js"></script>
+    <script src="/js/boostrap.min.js"></script>
+</html>
