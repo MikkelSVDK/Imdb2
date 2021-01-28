ab56a353601b02b9cc9e041359f901f668bf1b7e
Mikkel <Mikkel@DESKTOP-H8318GU>
2021-01-28 09:45:49 +0100

    @Added footer

---
+++ Includes/footer.php
@@ -0,0 +1,12 @@
+<footer class="footer">
+    <div class="bg-primary" style="top:unset;bottom:0;color:white;padding:0.5rem 0.9rem">
+        <center>
+            <a href="">HOME</a> | <a href="">NEWS</a> | <a href="">CONTACT</a><br>
+            <a href=""><i class="fab fa-facebook-square"></i></a>&nbsp;
+            <a href=""><i class="fab fa-twitter"></i></a>&nbsp;
+            <a href=""><i class="fab fa-instagram"></i></a>&nbsp;
+            <a href=""><i class="fab fa-youtube"></i></a><br>
+            © <?= date("Y") ?> IMDB2 Inc.
+        </center>
+    </div>
+</footer>
--- index.php
+++ index.php
@@ -5,10 +5,13 @@
         <title>Find alle dine favorit film | IMDB2</title>
         <link rel="stylesheet" href="/css/bootstrap.min.css">
         <link rel="stylesheet" href="/css/custom.css">
+        <link rel="stylesheet" href="https://mcskri.pt/css/sticky-footer.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
     </head>
     <body>
 <?php include("includes/navbar.php"); ?>
         Test
+<?php include("includes/footer.php"); ?>
     </body>
     <script src="/js/jquery.min.js"></script>
     <script src="/js/bootstrap.min.js"></script>
