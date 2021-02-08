a6c5fbc101f2b2ce59771449e686f80195ad3583
Jacob Marnil <jhmarnil@gmail.com>
2021-02-08 10:00:16 +0100

    @Movies Created

---
+++ movies.php
@@ -0,0 +1,71 @@
+<?php require("includes/core.php"); ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Find alle dine favorit film | IMDB2</title>
+        <link rel="stylesheet" href="/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/css/custom.css">
+        <link rel="stylesheet" href="https://mcskri.pt/css/sticky-footer.css">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+    </head>
+    <body>
+<?php include("includes/navbar.php"); ?>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-2">
+                    <img class="img-fluid" src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F4.bp.blogspot.com%2F_3N0VetpYvQE%2FSwxqEAu9_HI%2FAAAAAAAAATo%2Fs6D3g8096AQ%2Fs1600%2FBatman_The_Dark_Knight_23.jpg&f=1&nofb=1" alt="">
+                </div>
+                <div class="col-lg-2">
+                    <img class="img-fluid" src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F4.bp.blogspot.com%2F_3N0VetpYvQE%2FSwxqEAu9_HI%2FAAAAAAAAATo%2Fs6D3g8096AQ%2Fs1600%2FBatman_The_Dark_Knight_23.jpg&f=1&nofb=1" alt="">
+                </div>
+                <div class="col-lg-2">
+                    <img class="img-fluid" src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F4.bp.blogspot.com%2F_3N0VetpYvQE%2FSwxqEAu9_HI%2FAAAAAAAAATo%2Fs6D3g8096AQ%2Fs1600%2FBatman_The_Dark_Knight_23.jpg&f=1&nofb=1" alt="">
+                </div>
+                <div class="col-lg-2">
+                    <img class="img-fluid" src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F4.bp.blogspot.com%2F_3N0VetpYvQE%2FSwxqEAu9_HI%2FAAAAAAAAATo%2Fs6D3g8096AQ%2Fs1600%2FBatman_The_Dark_Knight_23.jpg&f=1&nofb=1" alt="">
+                </div>
+                <div class="col-lg-2">
+                    <img class="img-fluid" src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F4.bp.blogspot.com%2F_3N0VetpYvQE%2FSwxqEAu9_HI%2FAAAAAAAAATo%2Fs6D3g8096AQ%2Fs1600%2FBatman_The_Dark_Knight_23.jpg&f=1&nofb=1" alt="">
+                </div>
+                <div class="col-lg-2">
+                    <img class="img-fluid" src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F4.bp.blogspot.com%2F_3N0VetpYvQE%2FSwxqEAu9_HI%2FAAAAAAAAATo%2Fs6D3g8096AQ%2Fs1600%2FBatman_The_Dark_Knight_23.jpg&f=1&nofb=1" alt="">
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-lg-2">
+                    <img class="img-fluid" src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F4.bp.blogspot.com%2F_3N0VetpYvQE%2FSwxqEAu9_HI%2FAAAAAAAAATo%2Fs6D3g8096AQ%2Fs1600%2FBatman_The_Dark_Knight_23.jpg&f=1&nofb=1" alt="">
+                </div>
+                <div class="col-lg-2">
+                    <img class="img-fluid" src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F4.bp.blogspot.com%2F_3N0VetpYvQE%2FSwxqEAu9_HI%2FAAAAAAAAATo%2Fs6D3g8096AQ%2Fs1600%2FBatman_The_Dark_Knight_23.jpg&f=1&nofb=1" alt="">
+                </div>
+                <div class="col-lg-2">
+                    <img class="img-fluid" src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F4.bp.blogspot.com%2F_3N0VetpYvQE%2FSwxqEAu9_HI%2FAAAAAAAAATo%2Fs6D3g8096AQ%2Fs1600%2FBatman_The_Dark_Knight_23.jpg&f=1&nofb=1" alt="">
+                </div>
+                <div class="col-lg-2">
+                    <img class="img-fluid" src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F4.bp.blogspot.com%2F_3N0VetpYvQE%2FSwxqEAu9_HI%2FAAAAAAAAATo%2Fs6D3g8096AQ%2Fs1600%2FBatman_The_Dark_Knight_23.jpg&f=1&nofb=1" alt="">
+                </div>
+                <div class="col-lg-2">
+                    <img class="img-fluid" src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F4.bp.blogspot.com%2F_3N0VetpYvQE%2FSwxqEAu9_HI%2FAAAAAAAAATo%2Fs6D3g8096AQ%2Fs1600%2FBatman_The_Dark_Knight_23.jpg&f=1&nofb=1" alt="">
+                </div>
+                <div class="col-lg-2">
+                    <img class="img-fluid" src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F4.bp.blogspot.com%2F_3N0VetpYvQE%2FSwxqEAu9_HI%2FAAAAAAAAATo%2Fs6D3g8096AQ%2Fs1600%2FBatman_The_Dark_Knight_23.jpg&f=1&nofb=1" alt="">
+                </div>
+            </div>
+            
+            <Hr />
+            <div class="row">
+                <div class="col-lg-6">
+                    NEWS
+                </div>
+                <div class="col-lg-6">
+                    COMING SOON!
+                </div>
+            </div>
+        </div>
+
+<?php include("includes/footer.php"); ?>
+    </body>
+    <script src="/js/jquery.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+</html>
