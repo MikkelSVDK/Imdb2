564f0442a46d11d749c9c97615a1ffbb168cf3e0
Mikkel <Mikkel@DESKTOP-H8318GU>
2021-02-16 09:45:17 +0100

    @fixed img upload

--- actions/profile/updateimg.php
+++ actions/profile/updateimg.php
@@ -2,6 +2,6 @@
 require(realpath($_SERVER["DOCUMENT_ROOT"])."/includes/core.php");
 
 if($User != null){
-    $currentUser->SetImage($_FILES["img"]);
+    $User->SetImage($_FILES["img"]);
 }
 header("Location: $_SERVER[HTTP_REFERER]");
